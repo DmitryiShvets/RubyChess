4b5c1b99b0dd2a93ab6b89702a45546b8e177fce
unknown <dima.shvets95@inbox.ru>
2022-10-17 22:38:05 +0300

    feat: added game logic

--- Rakefile
+++ Rakefile
@@ -5,7 +5,7 @@ require "rake/testtask"
 
 Rake::TestTask.new(:test) do |t|
   t.libs << "test"
-  t.libs << "lib"
+  t.libs << "src"
   t.test_files = FileList["test/**/test_*.rb"]
 end
 
--- lib/chess.rb
+++ lib/chess.rb
@@ -1,13 +1,32 @@
 # frozen_string_literal: true
 
 require_relative "chess/version"
+require_relative 'src/invalid_move_error.rb'
+require_relative 'src/pieces.rb'
+require_relative 'src/board_renderer_text.rb'
+require_relative 'src/board.rb'
+require_relative 'src/player.rb'
+require_relative 'src/game.rb'
 
 module Chess
-  class Error < StandardError; end
+  class Error < StandardError;
+  end
 
-extend self
-  def hello
-    'Hello'
+
+  def start
+    startLogic()
+    return 'Game is starting'
+  end
+
+  def startLogic
+    b = Board.start_chess
+    g = Game.new(
+      b,
+      Player.new(:black),
+      Player.new(:white),
+      BoardRendererText
+    )
+    g.play
   end
 
 end
